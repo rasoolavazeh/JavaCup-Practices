67c20a3d5a2074f3072ded9eeb7542e2ef5812b1
rasoolavazeh <rasoolavazeh@yahoo.com>
2020-10-12 16:43:31 +0330

    Q2-CompleteNumbet

---
+++ Q2/src/CompleteNumber.java
@@ -0,0 +1,23 @@
+public class CompleteNumber {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i < 10000; i++) {
+            if (isComplete(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isComplete(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == num)
+            return true;
+        return false;
+    }
+}
